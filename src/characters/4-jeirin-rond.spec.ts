db3b40282d2af565577c58979cc2433a45072f18
Adam Reisinger <reisinger.adam@gmail.com>
2021-09-17 21:16:43 +0200

    Add jest to test.

---
+++ src/characters/4-jeirin-rond.spec.ts
@@ -0,0 +1,29 @@
+import { Fajok } from '../core/data/fajok';
+import { Hatterek } from '../core/data/hatterek';
+import { Iskolak } from '../core/data/iskolak';
+import { createKarakter } from '../core/karakter';
+import { prettyPrint } from '../display/pretty-print';
+
+test('Character: Jeirin Rond', () => {
+  const jeirin = createKarakter({
+    nev: 'Jeirin Rond',
+    hatterek: [Fajok.Ember, Iskolak.Tolvaj.Besurrano, Hatterek.PsziErzekenyseg, Hatterek.Manalato2, Hatterek.Vagyon1],
+    tulajdonsagok: {
+      ero: 13,
+      gyorsasag: 14,
+      ugyesseg: 15,
+      allokepesseg: 13,
+      karizma: 12,
+      egeszseg: 12,
+      intelligencia: 14,
+      akaratero: 14,
+      asztral: 13,
+      erzekeles: 15,
+    },
+    szintlepesek: [],
+  });
+  // első szint
+  // jeirin.szintlepes({});
+
+  console.log(prettyPrint(jeirin));
+});
